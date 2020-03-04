80532d24b79ea8465da9698c67827f58b4cb3a14
Chris Baran <possiblybob@gmail.com>
2020-03-03 22:39:54 -0600

    Begin admin forms for Item/Transaction models

--- items/admin.py
+++ items/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from items.models import Item, Transaction
+
+
+class TransactionInlineAdmin(admin.TabularInline):
+    """inline administration of Transactions"""
+    model = Transaction
+    extra = 1
+    show_change_link = True
+
+
+class ItemAdmin(admin.ModelAdmin):
+    """administration for Item objects"""
+    inlines = [
+        TransactionInlineAdmin,
+    ]
+
+
+admin.site.register(Item, ItemAdmin)
+admin.site.register(Transaction)
--- items/models.py
+++ items/models.py
@@ -6,34 +6,43 @@ from django.db import models
 
 class Item(models.Model):
     """a payment"""
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    create_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
     amount = models.BigIntegerField()
 
 
-class TransactionLocation(Enum):
+class TransactionEnum(Enum):
+    """common enumeration for Transaction values"""
+
+    @classmethod
+    def choices(cls):
+        return tuple((i.name, i.value) for i in cls)
+
+
+class TransactionLocation(TransactionEnum):
     """a given location for funds in a Transaction"""
-    ORIGINATION_BANK = 'Origination Bank'
-    ROUTABLE = 'Routable'
-    DESTINATION_BANK = 'Destination Bank'
+    originator_bank = 'Origination Bank'
+    routable = 'Routable'
+    destination_bank = 'Destination Bank'
 
 
-class TransactionStatus(Enum):
+class TransactionStatus(TransactionEnum):
     """a given status for funds in a Transaction"""
-    PROCESSING = 'Processing'
-    COMPLETED = 'Completed'
-    ERROR = 'Error'
+    processing = 'Processing'
+    completed = 'Completed'
+    error = 'Error'
 
 
 class Transaction(models.Model):
     """an action applied to an Item"""
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    create_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
     item = models.ForeignKey(Item, related_name='transactions', on_delete=models.CASCADE)
     status = models.CharField(
-        blank=True, null=True, max_length=20,
-        choices=[(tag, tag.value) for tag in TransactionStatus]
+        max_length=20, choices=TransactionStatus.choices(), default=TransactionStatus.processing
     )
     location = models.CharField(
-        blank=True, null=True, max_length=20,
-        choices=[(tag, tag.value) for tag in TransactionLocation]
+        max_length=20, choices=TransactionLocation.choices(), default=TransactionLocation.originator_bank
     )
-    create_date = models.DateTimeField(auto_now_add=True)
